da12c575936876a91cbc84bff6822ff24d4e4349
Joel Louine <49464653+YoJoGaming@users.noreply.github.com>
2019-04-12 15:18:44 -0400

    Update armlet.js

--- loc/items/armlet.js
+++ loc/items/armlet.js
@@ -3,7 +3,10 @@ export default [
     name       : {en: 'Simple Bracelet' },
     description: {
       en: 'Cheap leather band bought by an ambitious farmer',
-      ru: 'Дешевый кожаный ремешок, купленный амбициозным фермером'
+      ru: 'Дешевый кожаный ремешок, купленный амбициозным фермером',
+      es: 'Banda de cuero barata comprada por un granjero ambicioso',
+      de: 'Billiges Lederband, das von einem ehrgeizigen Bauern gekauft wurde.',
+      fr: 'Bracelet en cuir pas cher acheté par un fermier ambitieux'
     }
   },
 
@@ -11,7 +14,10 @@ export default [
     name       : {en: 'Makeshift Brace' },
     description: {
       en: 'A little more advanced, not by much though.',
-      ru: 'Немного более продвинутый, но не намного.'
+      ru: 'Немного более продвинутый, но не намного.',
+      es: 'Un poco más avanzado, aunque no mucho.',
+      de: 'Etwas fortgeschrittener, aber nicht viel.',
+      fr: 'Un peu plus avancé, mais pas de beaucoup.'
     }
   },
 
@@ -19,7 +25,10 @@ export default [
     name       : {en: 'Leather Armlet' },
     description: {
       en: 'Stiff, makes your arm sore.',
-      ru: 'Жосткая, болит рука.'
+      ru: 'Жосткая, болит рука.',
+      es: 'Rígido, hace que te duela el brazo.',
+      de: 'Steif, macht deinen Arm wund.',
+      fr: 'Rigide, ça te fait mal au bras.'
     }
   },
 
@@ -27,7 +36,10 @@ export default [
     name       : {en: 'Ivory Bracelet' },
     description: {
       en: 'Elegant cuff made from the tusks of the last mammonth.',
-      ru: 'Элегантная манжета из бивней последнего мамонта.'
+      ru: 'Элегантная манжета из бивней последнего мамонта.',
+      es: 'Elegante puño hecho de los colmillos del último mes.',
+      de: 'Elegante Manschette aus den Stoßzähnen des letzten Mammonats.',
+      fr: 'Manchette élégante fabriquée à partir des défenses du dernier mois.'
     }
   },
 
@@ -35,7 +47,10 @@ export default [
     name       : {en: 'Iron Vambrace' },
     description: {
       en: 'Heavy on your arm.....worth the protection.',
-      ru: 'Тяжело на твоей руке ..... стоит защиты.'
+      ru: 'Тяжело на твоей руке ..... стоит защиты.',
+      es: 'Pesado en tu brazo... vale la pena la protección.',
+      de: 'Schwer auf dem Arm.... wert den Schutz.',
+      fr: 'C\'est lourd sur ton bras.... ça vaut la peine d\'être protégé.
     }
   },
 
@@ -43,7 +58,10 @@ export default [
     name       : {en: 'Imbued Bracers' },
     description: {
       en: 'Dull metal bracer, handy for nightime raidings.',
-      ru: 'Унылый металлический браслет, удобный для ночных рейдов.'
+      ru: 'Унылый металлический браслет, удобный для ночных рейдов.',
+      es: 'El brazalete de metal mate, útil para las incursiones nocturnas.',
+      de: 'Stumpfer Metallarmschutz, praktisch für nächtliche Überfälle.',
+      fr: 'Bracelet en métal terne, pratique pour les raids de nuit.
     }
   },
 
@@ -51,7 +69,10 @@ export default [
     name       : {en: 'Ember Cuffs' },
     description: {
       en: 'Scaled bracer, makes your arms resemble that of a dragon.',
-      ru: 'Чешуйчатый наруч, делает ваши руки похожими на руки дракона.'
+      ru: 'Чешуйчатый наруч, делает ваши руки похожими на руки дракона.',
+      es: 'El brazalete a escala, hace que tus brazos se parezcan a los de un dragón.',
+      de: 'Schuppiger Armschutz, lässt deine Arme an die eines Drachens erinnern.',
+      fr: 'Bracelet à écailles, vos bras ressemblent à ceux d\'un dragon.'
     }
   },
 
@@ -59,7 +80,10 @@ export default [
     name       : {en: 'Mirrored Armlet' },
     description: {
       en: 'Blinds your foes with a magical shine off a devious magicians mirror......',
-      ru: 'Ослепляет ваших врагов волшебным блеском с коварного зеркалoм мага ......'
+      ru: 'Ослепляет ваших врагов волшебным блеском с коварного зеркалoм мага ......',
+      es: 'Ciega a tus enemigos con un brillo mágico de un espejo de magos taimados....',
+      de: 'Blinds Ihre Feinde mit einem magischen Glanz von einem teuflischen Magierspiegel.....',
+      fr: 'Aveugle vos ennemis avec un éclat magique sur un miroir de magiciens sournois....'
     }
   },
 
@@ -75,7 +99,10 @@ export default [
     name       : {en: 'Coldforged Bracer' },
     description: {
       en: 'The deceptively light steel offers chilling protection against sneaky blows',
-      ru: 'Обманчиво легкая сталь предлагает охлаждающую защиту от подлых ударов'
+      ru: 'Обманчиво легкая сталь предлагает охлаждающую защиту от подлых ударов',
+      es: 'Los guardianes arcaicos dejaron restos de su paso, que sin escrúpulos has decidido llevar en tu brazo.',
+      de: 'Die archaischen Wächter hinterließen Überreste ihres Todes, die du skrupellos auf deinem Arm getragen hast.',
+      fr: 'Les gardiens archaïques ont laissé des vestiges de leur mort, que vous avez décidé sans scrupules de porter sur votre bras.'
     }
   },
 
@@ -83,7 +110,10 @@ export default [
     name       : {en: 'Blackstar Gem' },
     description: {
       en: 'Stars are not often black, yet this fragment has a.......deep.......deep darkness.',
-      ru: 'Звезды не часто черные, но этот фрагмент имеет ....... глубокую ....... глубокую тьму.'
+      ru: 'Звезды не часто черные, но этот фрагмент имеет ....... глубокую ....... глубокую тьму.',
+      es: 'Las estrellas no suelen ser negras, pero este fragmento tiene una....profunda...profunda oscuridad.',
+      de: 'Sterne sind nicht oft schwarz, aber dieses Fragment hat eine...tiefe...tiefe...tiefe Dunkelheit.',
+      fr: 'Les étoiles ne sont pas souvent noires, mais ce fragment a une...profonde...profonde obscurité.'
     }
   },
 
@@ -91,7 +121,10 @@ export default [
     name       : {en: 'Eternal Vambraces' },
     description: {
       en: 'Reforged, lost, found, stolen, hidden for eras without end.',
-      ru: 'Перекован, потерян, найден, украден, спрятан на века без конца.'
+      ru: 'Перекован, потерян, найден, украден, спрятан на века без конца.',
+      es: 'Reforzado, perdido, encontrado, robado, escondido por eras sin fin.',
+      de: 'Aufgearbeitet, verloren, gefunden, gestohlen, versteckt für unendliche Zeiten.',
+      fr: 'Reforgé, perdu, retrouvé, volé, caché pour des époques sans fin.'
     }
   },
 
@@ -99,7 +132,10 @@ export default [
     name       : {en: 'Lost Guardian' },
     description: {
       en: 'This item was forged on a full moon when mysterious incantations were heard in the whispers of the dark. Some say that it was Anubis himself speaking with the dead.',
-      ru: 'Этот предмет был выкован в полнолуние, когда в шепот темноты раздавались таинственные заклинания. Некоторые говорят, что это был сам Анубис, говорящий с мертвыми.'
+      ru: 'Этот предмет был выкован в полнолуние, когда в шепот темноты раздавались таинственные заклинания. Некоторые говорят, что это был сам Анубис, говорящий с мертвыми.',
+      es: 'Este objeto se forjó en una luna llena cuando se escucharon misteriosos encantamientos en los susurros de la oscuridad. Algunos dicen que fue el propio Anubis quien habló con los muertos.',
+      de: 'Dieser Gegenstand wurde bei Vollmond geschmiedet, als im Flüstern der Dunkelheit mysteriöse Beschwörungen zu hören waren. Einige sagen, dass es Anubis selbst war, der mit den Toten sprach.',
+      fr: 'Cet objet a été forgé à la pleine lune lorsque de mystérieuses incantations ont été entendues dans les chuchotements de l\'obscurité. Certains disent que c\'était Anubis lui-même qui parlait avec les morts.
     }
   }
 
