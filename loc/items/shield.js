5f96f37dd601aa2bcbdaeaaebeb3f288ea9cc7d1
Joel Louine <49464653+YoJoGaming@users.noreply.github.com>
2019-04-12 13:53:29 -0400

    Update shield.js

--- loc/items/shield.js
+++ loc/items/shield.js
@@ -1,6 +1,6 @@
 export default [
   { // T1 item name. names are english only!
-    name       : { en: 'Trashy Thing' },
+    name       : { en: 'Wodden Shield' },
     // Item description
     description: {
       en: 'Interesting item description'
@@ -8,18 +8,31 @@ export default [
   },
 
   { // T2 item. names are english only!
-    name       : { en: 'Wooden Thing' },
+    name       : { en: 'Buckler' },
     description: {
       en: 'Interesting item description'
     }
   },
 
   { // T3 item. and so on...
-    name       : { en: 'Elven Thing' },
+    name       : { en: 'Old Bulwark' },
     description: {
       en: 'Interesting item description'
     }
-  }
+  },
 
-  // keep adding objects here for t4,t5, etc..
-]
+ { // T1 item name. names are english only!
+    name       : { en: 'Metal Guard' },
+    // Item description
+    description: {
+      en: 'Interesting item description'
+    }
+  },
+  
+   { // T1 item name. names are english only!
+    name       : { en: 'Protecteron' },
+    // Item description
+    description: {
+      en: 'Interesting item description'
+    }
+  ]
